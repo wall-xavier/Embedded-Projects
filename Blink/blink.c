eda82b024485acfe7e2d346f90b026ea4340fe5a
wall-xavier <xvwall@gmail.com>
2025-12-25 18:46:38 -0700

    Initial Commit

---
+++ Blink/blink.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void){
+
+    DDRB = DDRB | (1 << DDB7);
+
+    while(1){
+
+
+        PORTB = PORTB | (1 << PORTB7);
+
+        _delay_ms(5000);
+
+        PORTB = PORTB & ~(1 << PORTB7);
+
+        _delay_ms(5000);
+
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Profos-Embedded Systems
+
+This is the home to all of the Profos-Embedded Systems projects. Writting code for my Elegoo Board with a ATMEGA2560 Processor.
